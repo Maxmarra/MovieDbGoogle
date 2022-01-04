188aaa007fef862536f38ee8c9aa89b60a29830e
Maxmarra <5919669@gmail.com>
2022-01-04 13:20:21 +0300

    adding exception handling

--- app/src/main/java/com/example/moviedbgoogle/network/MovieApiService.kt
+++ app/src/main/java/com/example/moviedbgoogle/network/MovieApiService.kt
@@ -1,4 +1,28 @@
 package com.example.moviedbgoogle.network
 
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+private const val BASE_URL = "https://api.themoviedb.org/3/"
+private const val API_KEY="6f9ab437c43ec1fa9fb2dd9119d38c68"
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(ScalarsConverterFactory.create())
+    .baseUrl(BASE_URL)
+    .build()
+
 interface MovieApiService {
+    @GET("movie/popular?language=ru-RU")
+    suspend fun getMovies(
+        @Query("api_key") api_key: String = API_KEY
+    ) : String
+}
+
+object MovieApi {
+    val retrofitService : MovieApiService by lazy{
+        retrofit.create(MovieApiService::class.java)
+    }
 }
--- app/src/main/java/com/example/moviedbgoogle/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/moviedbgoogle/overview/OverviewViewModel.kt
@@ -3,25 +3,29 @@ package com.example.moviedbgoogle.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.moviedbgoogle.network.MovieApi
+import kotlinx.coroutines.launch
+import java.lang.Exception
 
 class OverviewViewModel : ViewModel() {
-    // The internal MutableLiveData that stores the status of the most recent request
-    private val _status = MutableLiveData<String>()
 
-    // The external immutable LiveData for the request status
+    private val _status = MutableLiveData<String>()
     val status: LiveData<String> = _status
-    /**
-     * Call getMarsPhotos() on init so we can display status immediately.
-     */
+
     init {
         getMoviePhotos()
     }
-
-    /**
-     * Gets Mars photos information from the Mars API Retrofit service and updates the
-     * [MoviePhoto] [List] [LiveData].
-     */
     private fun getMoviePhotos() {
-        _status.value = "Set the Movie API status response here!"
+        viewModelScope.launch {
+
+            try{
+                val listResult = MovieApi.retrofitService.getMovies()
+                _status.value = listResult
+            }catch (e: Exception){
+                _status.value = "Failure: ${e.message}"
+            }
+
+        }
     }
 }
